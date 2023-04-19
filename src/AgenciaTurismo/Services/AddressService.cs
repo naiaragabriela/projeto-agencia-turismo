5011840a7166b67912f28b4f15dfba531b92c683
naiaragabriela <naigabi31@gmail.com>
2023-04-18 22:38:14 -0300

    correcao Update e delete na AddressService

--- src/AgenciaTurismo/Services/AddressService.cs
+++ src/AgenciaTurismo/Services/AddressService.cs
@@ -41,9 +41,9 @@ namespace AgenciaTurismo.Services
                 commandInsert.Parameters.Add(new SqlParameter("@PostalCode", address.PostalCode));
                 commandInsert.Parameters.Add(new SqlParameter("@Description", address.Description));
                 commandInsert.Parameters.Add(new SqlParameter("@DtRegistration", address.DtRegistration));
-                commandInsert.Parameters.Add(new SqlParameter("@IdCity", new CityController().Insert(address.City))) ;
+                commandInsert.Parameters.Add(new SqlParameter("@IdCity", new CityController().Insert(address.City)));
 
-                
+
                 status = (int)commandInsert.ExecuteScalar();
             }
             catch (Exception)
@@ -74,7 +74,7 @@ namespace AgenciaTurismo.Services
             sb.Append("       City c");
             sb.Append("       where a.IdCity = c.Id");
 
-            
+
             SqlCommand commandSelect = new SqlCommand(sb.ToString(), conn);
             SqlDataReader dr = commandSelect.ExecuteReader();
 
@@ -87,7 +87,7 @@ namespace AgenciaTurismo.Services
                 address.Neighborhood = (string)dr["Neighborhood"];
                 address.Description = (string)dr["Description"];
                 address.DtRegistration = (DateTime)dr["DtRegsitration"];
-                address.City = new City() {Description = (string)dr["City"] };
+                address.City = new City() { Description = (string)dr["City"] };
 
                 addressList.Add(address);
             }
@@ -95,32 +95,33 @@ namespace AgenciaTurismo.Services
         }
 
 
-        //ver se está correto o UPDATE 
-        public int UpdateDescription(City city)
-            commandInsert.Parameters.Add(new SqlParameter("@Street", address.Street));
-                commandInsert.Parameters.Add(new SqlParameter("@Number", address.Number));
-                commandInsert.Parameters.Add(new SqlParameter("@Neighborhood", address.Neighborhood));
-                commandInsert.Parameters.Add(new SqlParameter("@PostalCode", address.PostalCode));
-                commandInsert.Parameters.Add(new SqlParameter("@Description", address.Description));
-                commandInsert.Parameters.Add(new SqlParameter("@DtRegistration", address.DtRegistration));
-                commandInsert.Parameters.Add(new SqlParameter("@IdCity", new CityController().Insert(address.Ci
-
-
+        public int Update(Address address)
 
         {
-            string _update = "update City set Description = @Description where Id = @id";
+            string _update = "update City set " +
+                 "Street = @Street" +
+                 "Number = @Number" +
+                 "Neighborhood = @Neighborhood" +
+                 "PostalCode = @PostalCode" +
+                 "Description = @Description" +
+                 "City = @IdCity" +
+                 " where Id = @id";
             SqlCommand commandUpdate = new SqlCommand(_update, conn);
+            commandUpdate.Parameters.Add(new SqlParameter("@Street", address.Street));
+            commandUpdate.Parameters.Add(new SqlParameter("@Number", address.Number));
+            commandUpdate.Parameters.Add(new SqlParameter("@Neighborhood", address.Neighborhood));
+            commandUpdate.Parameters.Add(new SqlParameter("@PostalCode", address.PostalCode));
             commandUpdate.Parameters.Add(new SqlParameter("@Description", address.Description));
-            commandUpdate.Parameters.Add(new SqlParameter("@Id", city.Id));
+            commandUpdate.Parameters.Add(new SqlParameter("@IdCity", address.City ));
 
             return commandUpdate.ExecuteNonQuery();
         }
 
-        public int DeleteId(City city)
+        public int Delete(Address address)
         {
             string _delete = "delete from Address where id =@id";
             SqlCommand commandDelete = new SqlCommand(_delete, conn);
-            commandDelete.Parameters.Add(new SqlParameter("@id", city.Id));
+            commandDelete.Parameters.Add(new SqlParameter("@id", address.Id));
 
             return (int)commandDelete.ExecuteNonQuery();
         }
